1a625da1b8dfb8d9bcffaf4036688f01a9f01c23
Berkay AY <61064047+berkays0733@users.noreply.github.com>
2022-09-16 12:17:01 -0700

    */

--- sayac.js
+++ sayac.js
@@ -1,27 +1,69 @@
-$(document).ready(function(){
-    var birth_date = new Date('May, 28, 2022').getTime();
-    
-    var years,months,days, hours, minutes, seconds;
-    var ageCount = document.getElementById('counter');
-    setInterval(function(){
-    
-        var current_date = new Date().getTime();
-        var total_sec =(current_date-birth_date) / 1000;
-        years=parseInt(total_sec/(86400*30*12));
-        var second_left=total_sec%(86400*30*12);
-        months=parseInt(second_left/(86400*30));
-        second_left=second_left%(86400*30);
-        days=parseInt(second_left/86400);
-        second_left=second_left%(86400);
-        hours=parseInt(second_left/3600);
-        second_left=second_left%3600;
-        minutes=parseInt(second_left/60);
-        seconds=parseInt(second_left%60);
-    
-    
-         ageCount.innerHTML=years+' Years '+months+' Months '+days+' Days '+hours+
-        ' Hours '+minutes+' Minutes '+seconds+' Seconds';
-    
-    },500);
-    
-    });
+function CountUp(initDate, id){
+    this.beginDate = new Date(initDate);
+    this.countainer =
+    document.getElementById(id);
+    this.numOfDays = [ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ];
+    this.borrowed = 0,
+    this.years = 0,
+    this.months = 0,
+    this.days = 0;
+    this.hours = 0,
+    this.minutes = 0,
+    this.seconds = 0;
+    this.updateNumOfDays();
+    this.updateCounter();
+}
+  
+CountUp.prototype.updateNumOfDays=function(){
+    var dateNow = new Date();
+    var currYear = dateNow.getFullYear();
+    if ( (currYear % 4 == 0 && currYear % 100 != 0 ) || currYear % 400 == 0 ) {
+        this.numOfDays[1] = 29;
+    }
+    var self = this;
+    setTimeout(function(){self.updateNumOfDays();}, (new Date((currYear+1), 1, 2) - dateNow));
+}
+  
+CountUp.prototype.datePartDiff=function(then, now, MAX){
+    var diff = now - then - this.borrowed;
+    this.borrowed = 0;
+    if ( diff > -1 ) return diff;
+    this.borrowed = 1;
+    return (MAX + diff);
+}
+  
+CountUp.prototype.calculate=function(){
+    var currDate = new Date();
+    var prevDate = this.beginDate;
+    this.seconds = this.datePartDiff(prevDate.getSeconds(), currDate.getSeconds(), 60);
+    this.minutes = this.datePartDiff(prevDate.getMinutes(), currDate.getMinutes(), 60);
+    this.hours = this.datePartDiff(prevDate.getHours(), currDate.getHours(), 24);
+    this.days = this.datePartDiff(prevDate.getDate(), currDate.getDate(), this.numOfDays[currDate.getMonth()]);
+    this.months = this.datePartDiff(prevDate.getMonth(), currDate.getMonth(), 12);
+    this.years = this.datePartDiff(prevDate.getFullYear(), currDate.getFullYear(),0);
+}
+  
+CountUp.prototype.addLeadingZero=function(value){
+    return value < 10 ? ("0" + value) : value;
+}
+  
+CountUp.prototype.formatTime=function(){
+    this.seconds = this.addLeadingZero(this.seconds);
+    this.minutes = this.addLeadingZero(this.minutes);
+    this.hours = this.addLeadingZero(this.hours);
+}
+
+CountUp.prototype.updateCounter=function(){
+    this.calculate();
+    this.formatTime();
+    this.countainer.innerHTML ="<strong>" + this.years + "</strong> <small>" + (this.years == 1? "Yıl" : "Yıl") + "</small>" +
+        " <strong>" + this.months + "</strong> <small>" + (this.months == 1? "Ay" : "Ay") + "</small>" +
+        " <strong>" + this.days + "</strong> <small>" + (this.days == 1? "Gün" : "Gün") + "</small>" +
+        " <strong>" + this.hours + "</strong> <small>" + (this.hours == 1? "Saat" : "Saat") + "</small>" +
+        " <strong>" + this.minutes + "</strong> <small>" + (this.minutes == 1? "Dakika" : "Dakika") + "</small>" +
+        " <strong>" + this.seconds + "</strong> <small>" + (this.seconds == 1? "Saniye" : "Saniye'dir beraberiz herşeyim") + "</small>";
+    var self = this;
+    setTimeout(function(){self.updateCounter();}, 1000);
+}
+ 
+window.onload=function(){ new CountUp('May 28, 2022 17:30:00', 'counter'); } // Başlangıç Saati - Start Time
